8fe9afc3d62affd01b52e7dc9d3c550ac14a79ee
Rony Derra <derrarony@gmail.com>
2022-04-14 20:14:58 +0300

    birthDate

--- Models/User.cs
+++ Models/User.cs
@@ -5,6 +5,7 @@ namespace homeworkS.Models
         public int Id { get; set; }
         public string NameH { get; set; }
         public string NameE { get; set; }
+        public string BirthDate { get; set; }
         public int PersonalId { get; set; }
         public string City { get; set; }
         public string Bank { get; set; }
